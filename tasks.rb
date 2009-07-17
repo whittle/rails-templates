8a3781d31922d9f72ecd0b21ad247fca77ded643
Jason Whittle <jason.whittle@gmail.com>
2009-07-17 19:16:05 -0400

    Generate common rake tasks.

---
+++ tasks.rb
@@ -0,0 +1,46 @@
+rakefile 'cache.rake' do
+  <<-'TASK'
+namespace :cache do
+	desc 'Clear the cache. Useful for implementations using memcached. '
+	task :clear => :environment do
+		Rails.cache.clear
+	end
+end
+  TASK
+end
+
+rakefile 'restart.rake' do
+  <<-'TASK'
+desc 'Restarts passenger instance, optionally with DEBUG. Once a debugger statement is reached, connect with "rdebug -c".'
+task :restart do
+	system('touch tmp/debug.txt') if ENV['DEBUG'] == 'true'
+	system('touch tmp/restart.txt')
+end
+  TASK
+end
+
+rakefile 'validate.rake' do
+  <<-'TASK'
+# Stolen shamelessly from http://blog.hasmanythrough.com/2006/8/27/validate-all-your-records
+namespace :db do
+  desc "Run model validations on all model records in database"
+  task :validate_models => :environment do
+    puts "-- records - model --"
+    Dir.glob(RAILS_ROOT + '/app/models/**/*.rb').each { |file| require file }
+    Object.subclasses_of(ActiveRecord::Base).select { |c| c.base_class == c}.sort_by(&:name).each do |klass|
+      total = klass.count
+      printf "%10d - %s\n", total, klass.name
+      chunk_size = 1000
+      (total / chunk_size + 1).times do |i|
+        chunk = klass.find(:all, :offset => (i * chunk_size), :limit => chunk_size)
+        chunk.reject(&:valid?).each do |record|
+          puts "#{record.class}: id=#{record.id}"
+          p record.errors.full_messages
+          puts
+        end rescue nil
+      end
+    end
+  end
+end
+  TASK
+end
