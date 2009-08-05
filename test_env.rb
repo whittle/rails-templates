be64b4326c4d41ec0d57e0b14f91ff8f61b57d27
Jason Whittle <jason.whittle@gmail.com>
2009-08-05 18:14:48 -0400

    New version of cucumber.

--- test_env.rb
+++ test_env.rb
@@ -1,12 +1,12 @@
-test_gems = <<DATA
+test_gems = <<CODE
 
 config.gem 'rspec', :lib => false, :version => '>=1.2.8'
 config.gem 'rspec-rails', :lib => false, :version => '>=1.2.7.1'
-config.gem 'cucumber', :version => '>=0.3.11'
+config.gem 'cucumber', :version => '>=0.3.93'
 config.gem 'webrat', :version => '>=0.4.4'
 config.gem 'spork', :lib => false, :version => '>=0.5.7'
 config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
-DATA
+CODE
 run "echo \"#{test_gems}\" >> config/environments/test.rb"
 rake 'gems:install', :env => 'test', :sudo => true
 generate 'cucumber', '--spork'
